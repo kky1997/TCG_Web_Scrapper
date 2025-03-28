e49e803a516aaf787df2a5ad9520dcd2be57b813
kky1997 <kaiyankoo@gmail.com>
2025-03-28 20:08:04 +1030

    test function for vue event handler

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="text-white d-flex align-items-center justify-content-center flex-column" style="height: 100vh;">
     <h1 class="fw-bold text-danger">I'm Chinese</h1>
-    <button class="btn btn-success mt-3">Pay Jason Wongstein some money</button>
+    <button class="btn btn-success mt-3" @click="doSomething">Pay Jason Wongstein some money</button>
   </div>
 </template>
 
 <script setup>
 import bg from '@/assets/background.jpg'
+
+function doSomething() {
+  alert("I think I may be a bit gay");
+}
 </script>
 
 <style scoped>
