d4a05c8667ebaa7785d3011e3907ad45f71902c2
kky1997 <kaiyankoo@gmail.com>
2025-04-03 16:06:43 +1030

    better commenting for runLocalServer

--- runLocalServer.sh
+++ runLocalServer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Run Vite dev server inside Docker (if you only want to start frontend)
+# -rm flag to remove container when stopped for clean dev loop.
+# IMPORTANT exec npm run dev, force dev (VITE as defined in the package.json), to run as the main process not a child process,
+# this ensures that when we ctrl+c to end the process, it directly ends the main VITE process, so we can ensure a clean server shutdown.
 docker run --rm -it \
   --name frontend-dev \
   -v "$PWD/frontend:/app" \
