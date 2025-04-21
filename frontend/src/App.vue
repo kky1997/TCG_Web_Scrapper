f982faeb0c970e1e24e934957fce5b85e479c57d
TiredOrc <ericrusso1996@gmail.com>
2025-04-21 15:50:23 +0930

    test1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,26 +19,13 @@ import RequestButton from './components/RequestButton.vue';
 const showImage = ref(false); // set showImage to false
 </script>
 
-<style scoped>
-.fullscreen-image {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-image: url('@/assets/maxresdefault.jpg');
-  background-size: cover;
-  background-position: center;
-  z-index: 9999;
-}
-</style>
 
 <style>
 /* Global background on body */
 body {
   background-image: url('@/assets/background.jpg');
   background-repeat: repeat;
-  background-size: 150px 150px;
+  background-size: 100px 150px;
   margin: 0; /* remove default margin */
   padding: 0;
 }
